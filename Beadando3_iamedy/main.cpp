d9d72f307ebe6a1b060ce99b74b77b0df4d00d42
zjambor <jambor.zoltan1@gmail.com>
2019-08-14 09:51:33 +0200

    ok

--- Beadando3_iamedy/main.cpp
+++ Beadando3_iamedy/main.cpp
@@ -188,6 +188,7 @@ int main(int argc, char const* argv[])
 	std::cout << "Kész" << endl;
 	std::cout << "Kész" << endl;
 	std::cout << "Kész" << endl;
+	std::cout << "Kész" << endl;
 
 	return 0;
 }
